d81958da3234fe4fda263a05810cba57c20dd1b0
Christian <christian@spitschka.name>
2020-01-01 22:20:32 +0100

    https://github.com/schuleintern/Webportal/issues/111

--- Upload/framework/lib/page/messages/MessageInbox.class.php
+++ Upload/framework/lib/page/messages/MessageInbox.class.php
@@ -20,6 +20,11 @@ class MessageInbox extends AbstractPage {
 		}
 		
 		$perPage = 20;
+
+		if(DB::getSettings()->getInteger('messages-inbox-messages-per-page') > 0) {
+		    $perPage = DB::getSettings()->getInteger('messages-inbox-messages-per-page');
+        }
+
 		$page = intval($_GET['pageNumber']);
 		
 		if($page > 0) {
@@ -262,10 +267,15 @@ class MessageInbox extends AbstractPage {
 	}
 	
 	public static function getSettingsDescription() {
-		$settings = [];
-		
-		
-		return $settings;
+		return [
+            [
+                'name' => "messages-inbox-messages-per-page",
+                'typ' => 'NUMMER',
+                'titel' => "Nachrichten pro Seite",
+                'text' => ""
+            ]
+        ];
+
 	}
 	
 	public static function getSiteDisplayName() {
@@ -273,45 +283,37 @@ class MessageInbox extends AbstractPage {
 	}
 	
 	public static function hasSettings() {
-		return false;
+		return true;
 	}
+
+	public static function hasAdmin() {
+	    return true;
+    }
 	
-	/**
-	 * Liest alle Nutzergruppen aus, die diese Seite verwendet. (Für die Benutzeradministration)
-	 * @return array(array('groupName' => '', 'beschreibung' => ''))
-	 */
-	public static function getUserGroups() {
-		return array();
-		
-	}
-	
+
 	public static function siteIsAlwaysActive() {
 		return true;
 	}
 	
-	public static function hasAdmin() {
-		return false;
-	}
-	
 	public static function getAdminGroup() {
-		return "NONE";
+		return "Webportal_Admin_Nachrichten_Inbox";
 	}
 	
 	public static function displayAdministration($selfURL) {
-		
+		return "";
 	}
-	
-	public static function getAdminMenuIcon() {
-		return 'fa fa-info-circle';
-	}
-	
-	public static function getAdminMenuGroupIcon() {
-		return 'fa fa-info-circle';
-	}
-	
-	public static function getAdminMenuGroup() {
-		return 'Schulinformationen';
-	}
+
+    public static function getAdminMenuIcon() {
+        return 'fa fa-comments';
+    }
+
+    public static function getAdminMenuGroupIcon() {
+        return 'fa fa-comments';
+    }
+
+    public static function getAdminMenuGroup() {
+        return 'Nachrichten';
+    }
 }
 
 
