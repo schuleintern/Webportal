f08d57e07222dba8f78d50118acc2b72a0688e76
Christian Marienfeld <post@chrisland.de>
2020-10-26 23:22:58 +0100

    ausleihe restyle

--- Upload/www/components/ausleihe/src/components/Form.vue
+++ Upload/www/components/ausleihe/src/components/Form.vue
@@ -1,44 +1,72 @@
 <template>
-  <form id="ausleihe-form" v-on:submit.prevent 
-    class="form box box-solid box-warning">
 
-    <div class="flexbox">
-      <h2>Reservieren</h2>
+
+  <div class="form-modal " v-on:click.self="handlerCloseModal" v-show="modalActive" >
+    <div class="form form-style-2 form-modal-content">
       
-      <input type="hidden" v-model="form.objektID"  required /> 
-      <input type="hidden" class="readonly" v-model="form.objektName" readonly required /> 
-      <input type="hidden" class="readonly" v-model="form.stunde" min="1" max="10" required />
-      <input type="hidden" class="readonly" v-model="form.datum[1]" readonly required/>
-
-      <ul>
-        <li>
-          <h4>am {{form.datum[0] | moment("Do MMMM YYYY")}} für die {{form.stunde}}. Stunde</h4>
-          <h5>{{form.objektName}}</h5>
-        </li>
-        <li>
-          <label>Klasse</label>
-          <input type="input" v-model="form.klasse" maxlength="10" required />  
-        </li>
-        <li>
-          <button @click="submitHandler"
-            class="btn btn-primary">Jetzt Reservieren</button>
-          <div v-if="errorMsg" class="errorMsg">{{errorMsg}}</div>
-        </li>
-      </ul>
-    </div>
-
-    <div class="flexbox">
-      <h3>Objekte</h3>
-      <div class="objects">
-        <button v-bind:key="index" v-for="(item, index) in objectsSelectable"
-        @click="setObjectHandler(item, $event)"
-        class="btn btn-info">
-          {{item.objektName}}
-        </button>
+      <div class="form-modal-close"v-on:click="handlerCloseModal"><i class="fa fa-times"></i></div>
+      
+      <div v-show="error" class="form-modal-error">
+        <b>Folgende Fehler sind aufgetreten:</b>
+        <ul>
+          <li>{{ error }}</li>
+        </ul>
       </div>
-    </div>
     
-  </form>
+      <h2>Reservieren</h2>
+
+      <h4>am {{form.datum[0] | moment("Do MMMM YYYY")}} für die {{form.stunde}}. Stunde</h4>
+      <h5>{{form.objektName}}</h5>
+
+      <form id="ausleihe-form" v-on:submit.prevent class="flex-row padding-t-m">
+
+        <input type="hidden" v-model="form.objektID"  required /> 
+        <input type="hidden" class="readonly" v-model="form.objektName" readonly required /> 
+        <input type="hidden" class="readonly" v-model="form.stunde" min="1" max="10" required />
+        <input type="hidden" class="readonly" v-model="form.datum[1]" readonly required/>
+
+
+        <div class="flex-1 margin-r-l">
+          <label class="block">Objekte</label>
+          <ul class="noListStyle">
+            <li v-bind:key="index" v-for="(item, index) in objectsSelectable"
+              class="margin-b-s" >
+              <button @click="setObjectHandler(item, $event)"
+                v-bind:class="isActive(item)"
+                class="btn btn-grau">
+                  {{item.objektName}}
+              </button>
+            </li>
+          </ul>
+        </div>
+
+        <div class="flex-1 width-30rem">
+          <ul class="noListStyle">
+            <li>
+              <label class="block">Klasse</label>
+              <input type="text" v-model="form.klasse" maxlength="10" required />  
+            </li>
+            <li class="margin-t-l">
+              <button @click="submitHandler"
+                class="btn btn-blau width-100p">Jetzt Reservieren</button>
+              <div v-if="errorMsg" class="errorMsg">{{errorMsg}}</div>
+            </li>
+          </ul>
+        </div>
+
+        
+      </form>
+
+
+
+      
+    </div>
+  </div>
+
+
+
+      
+
 </template>
 
 <script>
@@ -52,6 +80,9 @@ export default {
   },
   data: function () {
     return {
+      modalActive: false,
+      error: false,
+
       form: {
         'datum': '',
         'klasse': '',
@@ -61,7 +92,7 @@ export default {
         'sub': ''
       },
       objects: globals.objects,
-      selectedObject: false
+      //selectedObject: false
     }
   },
 
@@ -90,13 +121,15 @@ export default {
     var that = this;
     EventBus.$on('form--open', data => {
 
-      //document.getElementById("ausleihe-form").style.display = 'flex';
-      document.getElementById("ausleihe-form").classList.add('show');
+      that.modalActive = true;
 
-      // jQuery
-      $([document.documentElement, document.body]).animate({
-          scrollTop: $("#ausleihe-form").offset().top
-      }, 600);
+      // //document.getElementById("ausleihe-form").style.display = 'flex';
+      // document.getElementById("ausleihe-form").classList.add('show');
+
+      // // jQuery
+      // $([document.documentElement, document.body]).animate({
+      //     scrollTop: $("#ausleihe-form").offset().top
+      // }, 600);
 
 
       for (var foo in data) {
@@ -105,10 +138,10 @@ export default {
 
       that.form['objektID'] = false;
       that.form['objektName'] = '';
-      if (that.selectedObject) {
-        that.selectedObject.classList.remove('active');
-      }
-      that.selectedObject = false;
+      // if (that.selectedObject) {
+      //   that.selectedObject.classList.remove('active');
+      // }
+      // that.selectedObject = false;
 
       // EventBus.$emit('form--check', this.form);
 
@@ -117,16 +150,18 @@ export default {
     EventBus.$on('form--close', data => {
 
       //document.getElementById("ausleihe-form").style.display = 'none';
-      document.getElementById("ausleihe-form").classList.remove('show');
+      //document.getElementById("ausleihe-form").classList.remove('show');
+
+      this.modalActive = false;
 
       for (var foo in that.form) {
         that.form[foo] = '';
       }
       
-      if (that.selectedObject) {
-        that.selectedObject.classList.remove('active');
-      }
-      that.selectedObject = false;
+      // if (that.selectedObject) {
+      //   that.selectedObject.classList.remove('active');
+      // }
+      // that.selectedObject = false;
 
       document.getElementById("ausleihe-form").reset();
 
@@ -135,22 +170,26 @@ export default {
   },
   methods: {
 
+    isActive: function (item) {
+      if ( parseInt(item.objektID) == this.form.objektID ) {
+        return 'active';
+      }
+      return false;
+    },
     setObjectHandler: function (object, event) {
 
-      if ( event.target.classList.contains('disable')) {
-        return false;
-      }
+
       if (object.objektID) {
         this.form.objektID = object.objektID;
         this.form.objektName = object.objektName;
         this.form.sub = object.sub || false;
       }
 
-      if (this.selectedObject) {
-        this.selectedObject.classList.remove('active');
-      }
-      this.selectedObject = event.target
-      this.selectedObject.classList.add('active');
+      // if (this.selectedObject) {
+      //   this.selectedObject.classList.remove('active');
+      // }
+      // this.selectedObject = event.target
+      // this.selectedObject.classList.add('active');
 
       event.preventDefault();
 
@@ -165,15 +204,19 @@ export default {
       }
       if (!this.form.stunde) { return false; }
 
-      if (this.selectedObject) {
-        this.selectedObject.classList.remove('active');
-      }
+      // if (this.selectedObject) {
+      //   this.selectedObject.classList.remove('active');
+      // }
 
       this.form.datum = this.form.datum[1]; // war nur fuer frontend
 
       EventBus.$emit('form--submit', this.form);
 
-    }
+    },
+
+    handlerCloseModal: function () {
+      this.modalActive = false;
+    },
 
   }
 }
