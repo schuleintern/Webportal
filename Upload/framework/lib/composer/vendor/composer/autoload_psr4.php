76b324edc0ad69de72088ffa7e1bbfe08f756044
Christian <info@spitschka.com>
2020-10-26 14:01:49 +0100

    Composer Update

--- Upload/framework/lib/composer/vendor/composer/autoload_psr4.php
+++ Upload/framework/lib/composer/vendor/composer/autoload_psr4.php
@@ -17,7 +17,6 @@ return array(
     'Mpdf\\' => array($vendorDir . '/mpdf/mpdf/src'),
     'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
     'League\\OAuth2\\Client\\' => array($vendorDir . '/league/oauth2-client/src'),
-    'KasApi\\' => array($vendorDir . '/wazaari/kasapi-php/src/KasApi'),
     'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
     'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
     'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
