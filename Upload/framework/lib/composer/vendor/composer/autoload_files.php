c8feeb7303d7f949a72ee5f7de6452e30986f19b
Christian <christian@spitschka.name>
2020-12-07 10:21:48 +0100

    htmlpurifier composer Modul hinzugefügt

--- Upload/framework/lib/composer/vendor/composer/autoload_files.php
+++ Upload/framework/lib/composer/vendor/composer/autoload_files.php
@@ -14,4 +14,5 @@ return array(
     '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
     'a4ecaeafb8cfb009ad0e052c90355e98' => $vendorDir . '/beberlei/assert/lib/Assert/functions.php',
     '6124b4c8570aa390c21fafd04a26c69f' => $vendorDir . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
+    '2cffec82183ee1cea088009cef9a6fc3' => $vendorDir . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
 );
