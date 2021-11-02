fc08c804df8d99cfbbd321888f8ed44403cd8b49
Christian Marienfeld <post@zwiebelgasse.de>
2021-11-02 02:02:42 +0100

    Update.php !! DB Schema Aenderung!

--- Datenbank/Schemaaenderungen/menu_0.0.1.sql
+++ Datenbank/Schemaaenderungen/menu_0.0.1.sql
@@ -18,6 +18,7 @@ CREATE TABLE `menu_item` (
      `active` tinyint(1) DEFAULT '0',
      `menu_id` int(11) NOT NULL,
      `parent_id` int(11) NOT NULL,
+     `sort` int(3) DEFAULT '0',
      `page` varchar(100) DEFAULT '',
      `title` varchar(100) NOT NULL DEFAULT '',
      `icon` varchar(100) DEFAULT NULL,
@@ -26,15 +27,15 @@ CREATE TABLE `menu_item` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-INSERT INTO `menu_item` (`id`, `active`, `menu_id`, `parent_id`, `page`, `title`, `icon`, `params`)
+INSERT INTO `menu_item` (`id`, `active`, `menu_id`, `parent_id`, `sort`, `page`, `title`, `icon`, `params`)
 VALUES
-    (1,1,1,0,'','Aktuelles','fa fa-clock',NULL),
-    (2,1,1,0,'','Informationen','fa fa-clock',NULL),
-    (3,1,1,0,'','Lehreranwendungen','fa fa-graduation-cap',NULL),
-    (4,1,1,0,'','Verwaltung','fa fas fa-pencil-alt-square',NULL),
-    (5,1,1,0,'','Benutzeraccount / Nachrichten','fa fa-user',NULL),
-    (6,1,1,0,'','Unterricht','fa fa-graduation-cap',NULL),
-    (7,1,1,0,'','Administration','fa fa-cogs',NULL);
+    (1,1,1,0,0,'','Aktuelles','fa fa-clock',NULL),
+    (2,1,1,0,0,'','Informationen','fa fa-clock',NULL),
+    (3,1,1,0,0,'','Lehreranwendungen','fa fa-graduation-cap',NULL),
+    (4,1,1,0,0,'','Verwaltung','fa fas fa-pencil-alt-square',NULL),
+    (5,1,1,0,0,'','Benutzeraccount / Nachrichten','fa fa-user',NULL),
+    (6,1,1,0,0,'','Unterricht','fa fa-graduation-cap',NULL),
+    (7,1,1,0,0,'','Administration','fa fa-cogs',NULL);
 
 
 
