c6124f162843f9e274c78c7cccf395b62e04e477
Christian Marienfeld <post@zwiebelgasse.de>
2023-01-10 22:33:26 +0100

    boilerplate

---
+++ Boilerplate/vue/mixins/FormNumber.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <input type="number" v-model="value" v-on:change="handlerToggle" />
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      value: 0
+    };
+  },
+  props: {
+    input: Number,
+    disable: Number
+  },
+  created: function () {
+    this.value = this.input;
+  },
+  methods: {
+    handlerToggle: function () {
+      if (this.disable == 0) { // AUS
+        return false;
+      }
+      this.$emit('submit', {
+        value: parseInt(this.value)
+      });
+    }
+  }
+
+
+};
+</script>
+
+<style>
+
+</style>
