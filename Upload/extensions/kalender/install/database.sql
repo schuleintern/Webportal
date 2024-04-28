60fe63f4d0b51fb99a5fca2a1547d4f3699d8352
Christian Marienfeld <post@zwiebelgasse.de>
2024-04-28 22:39:49 +0200

    ext_kalender: update databas

--- Upload/extensions/kalender/install/database.sql
+++ Upload/extensions/kalender/install/database.sql
@@ -1,21 +1,21 @@
-
 CREATE TABLE `ext_kalender`
 (
-    `id`        int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `id`        int(11) UNSIGNED NOT NULL,
     `title`     varchar(255) NOT NULL DEFAULT '',
-    `state`     tinyint(1) DEFAULT '1',
+    `state`     tinyint(1) DEFAULT 1,
     `color`     varchar(7)            DEFAULT NULL,
     `sort`      tinyint(1) DEFAULT NULL,
     `preSelect` tinyint(1) DEFAULT NULL,
     `acl`       int(11) DEFAULT NULL,
-    `ferien`    tinyint(1) DEFAULT '0',
+    `ferien`    tinyint(1) DEFAULT 0,
     `public`    tinyint(1) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 CREATE TABLE `ext_kalender_events`
 (
-    `id`           int(11) NOT NULL AUTO_INCREMENT,
+    `id`           int(11) NOT NULL,
+    `status`       tinyint(4) DEFAULT 0,
     `kalender_id`  int(11) NOT NULL,
     `title`        varchar(255) NOT NULL DEFAULT '',
     `dateStart`    date                  DEFAULT NULL,
@@ -23,11 +23,21 @@ CREATE TABLE `ext_kalender_events`
     `dateEnd`      date                  DEFAULT NULL,
     `timeEnd`      time                  DEFAULT NULL,
     `place`        varchar(255)          DEFAULT '',
-    `comment`      text,
+    `comment`      text                  DEFAULT NULL,
     `user_id`      int(11) NOT NULL,
     `createdTime`  datetime     NOT NULL,
     `modifiedTime` datetime              DEFAULT NULL,
     `repeat_type`  varchar(10)           DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1496 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+
+
+CREATE TABLE `ext_kalender_ics`
+(
+    `id`          int(11) UNSIGNED NOT NULL,
+    `createdTime` date         DEFAULT NULL,
+    `user_id`     int(11) DEFAULT NULL,
+    `keyCode`     varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci ROW_FORMAT=DYNAMIC;
